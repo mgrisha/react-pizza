515a73bd76567d842ebeba831e7d9e6618d1f18c
User_g <grisha.endorphone@gmail.com>
2023-02-06 23:39:01 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import ProductBlock from "./components/ProductBlock";
+
+import './scss/app.scss';
+
 function App() {
   return (
-    <div>
-      Pizza Shop
-    </div>
+      <div className="wrapper">
+          <Header />
+          <div className="content">
+              <div className="container">
+                  <div className="content__top">
+                      <Categories />
+                      <Sort />
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                      <ProductBlock />
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
